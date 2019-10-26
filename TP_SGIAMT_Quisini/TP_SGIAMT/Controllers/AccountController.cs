44b4b45d069a79c8b2e8042d5f2e1703acaf1e3b
alvaro santisteban <alvarosantisteban56@gmail.com>
2019-10-25 22:14:46 -0500

    queso

---
+++ TP_SGIAMT_Quisini/TP_SGIAMT/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP_SGIAMT.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
